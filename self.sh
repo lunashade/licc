4a39cf5da92ae321307c3f019fdc243eefa87880
lunashade <word47wonders@gmail.com>
2020-05-08 15:15:27 +0900

    Self host except C preprocessor

--- self.sh
+++ self.sh
@@ -11,6 +11,12 @@ typedef struct FILE FILE;
 extern FILE *stdin;
 extern FILE *stdout;
 extern FILE *stderr;
+typedef struct {
+  int gp_offset;
+  int fp_offset;
+  void *overflow_arg_area;
+  void *reg_save_area;
+} va_list[1];
 
 void *malloc(long size);
 void *calloc(long nmemb, long size);
@@ -25,14 +31,24 @@ static void assert() {}
 int strcmp(char *s1, char *s2);
 int printf(char *fmt, ...);
 int sprintf(char *buf, char *fmt, ...);
+int fprintf(FILE *fp, char *fmt, ...);
+int vfprintf(FILE *fp, char *fmt, va_list ap);
 long strlen(char *p);
 int strncmp(char *p, char *q);
+int strncat(char *p, char *q, int n);
+int strncasecmp(char *p, char *q, int n);
 void *memcpy(char *dst, char *src, long n);
 char *strndup(char *p, long n);
 int isspace(int c);
 int iscntrl(int c);
 int ispunct(int c);
+int isdigit(int c);
+int isalpha(int c);
+int isalnum(int c);
 char *strstr(char *haystack, char *needle);
+static void va_end(va_list ap) {}
+long strtoul(char *nptr, char **endptr, int base);
+void exit(int code);
 EOF
 
     grep -v '^#' lcc.h >> $TMP/$1
@@ -41,7 +57,7 @@ EOF
     sed -i 's/\berrno\b/*__errno_location()/g' $TMP/$1
     sed -i 's/\btrue\b/1/g; s/\bfalse\b/0/g;' $TMP/$1
     sed -i 's/\bNULL\b/0/g' $TMP/$1
-    sed -i 's/, \.\.\.//g' $TMP/$1
+    sed -i 's/\bva_start\b/__builtin_va_start/g' $TMP/$1
 
     ./lcc $TMP/$1 > $TMP/${1%.c}.s
     gcc -c -o $TMP/${1%.c}.o $TMP/${1%.c}.s
@@ -53,8 +69,8 @@ cc() {
 
 lcc main.c
 lcc typing.c
-cc parse.c
+lcc parse.c
 lcc codegen.c
-cc tokenize.c
+lcc tokenize.c
 
 gcc -static -o lcc-stage2 $TMP/*.o
