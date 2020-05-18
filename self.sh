709220a96da7d2da5775ca66fd473b046a4f4718
lunashade <word47wonders@gmail.com>
2020-05-18 23:38:51 +0900

    Clean up self.sh

--- self.sh
+++ self.sh
@@ -5,86 +5,17 @@ TMP=$1
 CC=$2
 OUT=$3
 
-HEADER=lcc.h
-
 rm -rf $TMP
 mkdir -p $TMP
 
-lcc-header() {
-    cat <<EOF > $TMP/$1
-typedef struct FILE FILE;
-struct stat {
-  char _[512];
-};
-extern FILE *stdin;
-extern FILE *stdout;
-extern FILE *stderr;
-typedef struct {
-  int gp_offset;
-  int fp_offset;
-  void *overflow_arg_area;
-  void *reg_save_area;
-} va_list[1];
-
-void *malloc(long size);
-void *calloc(long nmemb, long size);
-void *realloc(void *buf, long size);
-int *__errno_location();
-char *strerror(int errnum);
-FILE *fopen(char *pathname, char *mode);
-long fread(void *ptr, long size, long nmemb, FILE *stream);
-int fclose(FILE *fp);
-int feof(FILE *stream);
-static void assert() {}
-int strcmp(char *s1, char *s2);
-int printf(char *fmt, ...);
-int sprintf(char *buf, char *fmt, ...);
-int fprintf(FILE *fp, char *fmt, ...);
-int vfprintf(FILE *fp, char *fmt, va_list ap);
-long strlen(char *p);
-int strncmp(char *p, char *q);
-int strncat(char *p, char *q, int n);
-int strncasecmp(char *p, char *q, int n);
-void *memcpy(char *dst, char *src, long n);
-char *strndup(char *p, long n);
-int isspace(int c);
-int iscntrl(int c);
-int ispunct(int c);
-int isdigit(int c);
-int isalpha(int c);
-int isalnum(int c);
-char *strstr(char *haystack, char *needle);
-static void va_end(va_list ap) {}
-long strtoul(char *nptr, char **endptr, int base);
-char *strchr(char *s, char c);
-char *strncpy(char *s1, const char *s2, int n);
-double strtod(char *s, char **endptr);
-void exit(int code);
-int stat(char *path, struct stat *statbuf);
-EOF
-    grep -v '^#' $1 >> $TMP/$1
-    sed -i 's/\bbool\b/_Bool/g' $TMP/$1
-    sed -i 's/\berrno\b/*__errno_location()/g' $TMP/$1
-    sed -i 's/\btrue\b/1/g; s/\bfalse\b/0/g;' $TMP/$1
-    sed -i 's/\bNULL\b/0/g' $TMP/$1
-    sed -i 's/\bva_start\b/__builtin_va_start/g' $TMP/$1
-
-}
-
 lcc() {
-    $CC src/$1 -c -o $TMP/${1%.c}.o
+    src=$1
+    obj=$(basename "${src%.c}.o")
+    $CC -c -o $TMP/${obj} $src
 }
 
-cc() {
-    gcc -c -o $TMP/${1%.c}.o $1
-}
-
-lcc main.c
-lcc typing.c
-lcc parse.c
-lcc codegen.c
-lcc tokenize.c
-lcc preprocess.c
-lcc debug.c
+for f in src/*.c; do
+    lcc "$f"
+done
 
 (cd $TMP; gcc -o ../$OUT *.o)
